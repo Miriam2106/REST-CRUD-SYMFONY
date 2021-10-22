396a47dfeb7eb7896d05e9c9386edfab23ec3830
miriam2106 <20203tn055@utez.edu.mx>
2021-10-21 20:30:08 -0500

    REST CRUD SYMFONY

---
+++ README.md
@@ -0,0 +1,1 @@
+#REST CRUD SYMFONY, una disculpa x no poner atención hoy a su clase, pero era de vida o muerte
---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+header('Access-Control-Allow-Origin: *');
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+// Json response
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ProductsController extends AbstractController
+{
+    public function findAllProducts()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery('SELECT p.idproduct, p.name, p.price, p.status FROM App:Products p');
+        $listProducts = $query->getResult();
+
+        $data = [
+            'status'=> 404,
+            'message'=> 'No se encontraron resultados'
+        ];
+
+        if(count( $listProducts) > 0){
+            $data = [
+                'status'=> 200,
+                'message'=> 'Se encontraron ' . count( $listProducts) . ' resultados',
+                'listProducts' => $listProducts
+            ];
+        }
+        
+        //devuelve un json
+        return new JsonResponse($data);
+    }
+}
